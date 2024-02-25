66994d7a5142bcccb8da66dd7a5efc5777705214
Jai Sakalle <jaisakalle@gmail.com>
2024-02-26 00:58:40 +0530

    initial commit

---
+++ py/web/script.js
@@ -0,0 +1,5 @@
+function Startscandata(){
+    eel.helloworld()((setdata)=>{
+        document.getElementById("heading").innerText=setdata;
+    })
+}
---
+++ web/script.js
@@ -0,0 +1,5 @@
+function Startscandata(){
+    eel.helloworld()((setdata)=>{
+        document.getElementById("btnActive").innerText=setdata;
+    })
+}
